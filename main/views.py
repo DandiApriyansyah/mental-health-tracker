f4f4332cf5612bed0db16fc50507c930e35e069d
DandiApriyansyah <apriyansyahdandi@gmail.com>
2024-09-16 16:37:57 +0700

    Create New Project mentalhealthtracker

--- main/views.py
+++ main/views.py
@@ -10,9 +10,9 @@ def show_main(request):
     mood_entries = MoodEntry.objects.all()
 
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP D',
-        'npm': '2306123456',
+        'name': 'Dandi Apriyansyah',
+        'class': 'PBP A',
+        'npm': '2306165843',
         'mood_entries': mood_entries
     }
 
